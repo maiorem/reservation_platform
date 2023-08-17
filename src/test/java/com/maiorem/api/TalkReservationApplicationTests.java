58dc8c473397a4bddc616d17b52b28bd250867aa
Seyoung Hong <sehong@nuskin.com>
2023-08-17 17:20:55 +0900

    kickoff(#1) add source

---
+++ src/test/java/com/maiorem/api/TalkReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maiorem.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalkReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
